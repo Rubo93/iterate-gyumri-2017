54271282c5a3d4311f7bffc9040cfe24ae914586
Rubo93 <ruboazizbekyan@gmail.com>
2017-05-18 20:54:59 +0400

    ev Changes

--- ev.js
+++ ev.js
@@ -1,12 +1,16 @@
 'use strict';
-let counter = 0;
+//let counter = 0;
 //use clearInterval to stop the
 //setInterval
 //use if(condition){}else{}
-const interval = setInterval(function(){
-  console.log(`Called ${++counter} times`);
-  if (counter ==8){
-      clearInterval(interval);
-  }
+//const interval = setInterval(function(){
+  //console.log(`Called ${++counter} times`);
+  //if (counter ==8){
+    //  clearInterval(interval);
+  //}
 
-},500);
+//},500);
+process.stdin.on('data',function(d){
+  console.log(d);
+  console.log(d.toString().toUpperCase())
+})
