374ccec66f82ab41113fd4a0ded3d8e3c24ff7e5
Rubo93 <ruboazizbekyan@gmail.com>
2017-05-18 20:28:05 +0400

    stop interval

--- ev.js
+++ ev.js
@@ -1,4 +1,12 @@
 'use strict';
-setInterval(function(){
-  console.log('Hello with delay:)')
-},3000);
+let counter = 0;
+//use clearInterval to stop the
+//setInterval
+//use if(condition){}else{}
+const interval = setInterval(function(){
+  console.log(`Called ${++counter} times`);
+  if (counter ==8){
+      clearInterval(interval);
+  }
+
+},500);
